99d718e48019ff436f3dc3791af1d6c8dfbb6c69
mllover5901 <74198115+mllover5901@users.noreply.github.com>
2021-03-10 12:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,65 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  2 22:51:42 2021
-
-@author: nibed
-"""
-
+import os, sys
 from flask import Flask, request
+from utils import wit_response
 from pymessenger import Bot
 
-app = Flask("My echo bot")
+app = Flask("My echo Bot")
 
-FB_ACCESS_TOKEN = "EAATzGVXlz7YBAHYTQLi8EfEzFOnKrKo3RbDZBXStcE4t8TWvZC0OC8VLYqquJFIuu1rMfkxix8dCMbgXgUxXYL80KxiKKpAK1uIhZBT9jMskh2LxCGsVZAsV9RhdM2XPI8aDIzUYyPzwZCf8bGPQHQ2VIJwkkEU9FonrQHJae3QZDZD"
-bot = Bot(FB_ACCESS_TOKEN)
+PAGE_ACCESS_TOKEN = "EAATzGVXlz7YBAHYTQLi8EfEzFOnKrKo3RbDZBXStcE4t8TWvZC0OC8VLYqquJFIuu1rMfkxix8dCMbgXgUxXYL80KxiKKpAK1uIhZBT9jMskh2LxCGsVZAsV9RhdM2XPI8aDIzUYyPzwZCf8bGPQHQ2VIJwkkEU9FonrQHJae3QZDZD"
 
-VERIFICATION_TOKEN = "hello"
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 
 @app.route('/', methods=['GET'])
 def verify():
-	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-		if not request.args.get("hub.verify_token") == VERIFICATION_TOKEN:
-			return "Verification token mismatch", 403
-		return request.args["hub.challenge"], 200
-	return "Hello world", 200
+	# Webhook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-	print(request.data)
 	data = request.get_json()
+	log(data)
 
-	if data['object'] == "page":
-		entries = data['entry']
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
 
-		for entry in entries:
-			messaging = entry['messaging']
-
-			for messaging_event in messaging:
-
+				# IDs
 				sender_id = messaging_event['sender']['id']
 				recipient_id = messaging_event['recipient']['id']
 
 				if messaging_event.get('message'):
-					# HANDLE NORMAL MESSAGES HERE
-					if messaging_event['message'].get('text'):
-						# HANDLE TEXT MESSAGES
-						query = messaging_event['message']['text']
-						# ECHO THE RECEIVED MESSAGE
-						bot.send_text_message(sender_id, query)
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'no text'
+
+					response = None
+
+					entity, value = wit_response(messaging_text)
+					if entity == 'location':
+						response = "Ok, I will send you the weather of {}".format(str(value))
+					elif entity == 'weather':
+						response = "Ok, I will send you the {} of India".format(str(value))
+
+					if response == None:
+						response = "I have no idea what you are saying!"
+						
+					bot.send_text_message(sender_id, response)
+
 	return "ok", 200
 
 
+def log(message):
+	print(message)
+	sys.stdout.flush()
+
+
 if __name__ == "__main__":
-	app.run(port=80, use_reloader = True)
+	app.run(debug = True, port = 80)
