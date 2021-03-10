0b4e1f9ee69cca21c827f23c842e73599ee270f9
mllover5901 <74198115+mllover5901@users.noreply.github.com>
2021-03-10 18:35:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,40 +14,14 @@ client = Wit(access_token = access_token)
 
 def wit_response(message_text):
 	resp = client.message(message_text)
-	categories = {'newstype:newstype':None, 'wit$location:location':None}
-
 	
-	entities = list(resp['entities'])
-	for entity in entities:
-		categories[entity] = resp['entities'][entity][0]['value']
-	
-	return categories
+	entity = None
+	value = None
 
+	try:
+		entity = list(resp['entities'])[0]
+		value = resp['entities'][entity][0]['value']
+	except:
+		pass
 
-def get_news_elements(categories):
-	news_client = gnewsclient()
-	news_client.query = ''
-
-	if categories['newstype:newstype'] != None:
-		news_client.query += categories['newstype:newstype'] + ' '
-
-	if categories['wit$location:location'] != None:
-		news_client.query += categories['wit$location:location']
-
-	news_items = news_client.get_news()
-
-	elements = []
-
-	for item in news_items:
-		element = {
-					'title': item['title'],
-					'buttons': [{
-								'type': 'web_url',
-								'title': "Read more",
-								'url': item['link']
-					}],
-					'image_url': item['img']		
-		}
-		elements.append(element)
-
-	return elements
+	return (entity, value)
